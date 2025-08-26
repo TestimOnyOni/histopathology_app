c40c64eefc1f440eb9079b73360d7f1453d82e8a
TestimOnyOni <olufayooni43@gmail.com>
2025-08-26 19:41:24 +0100

    Model loading issues

--- backend/config.py
+++ backend/config.py
@@ -1,20 +1,3 @@
-# from pathlib import Path
-
-# # Paths
-# BASE_DIR = Path(__file__).resolve().parent.parent
-# MODEL_DIR = BASE_DIR / "models"
-
-# # Model files
-# MODEL_PATH = MODEL_DIR / "best_resnet50_balanced.pth"
-# THR_PATH = MODEL_DIR / "deploy_resnet50_slide_threshold.json"
-
-# # Model download URL (use raw GitHub link, not blob!)
-# MODEL_URL = "https://raw.githubusercontent.com/TestimOnyOni/histopathology_app/main/models/best_resnet50_balanced.pth"
-
-# # Inference settings
-# DEFAULT_NUM_CLASSES = 2
-
-
 from pathlib import Path
 
 # Paths inside the container
@@ -23,6 +6,6 @@ MODEL_PATH = BASE_DIR / "models" / "best_resnet50_balanced.pth"
 THR_PATH = BASE_DIR / "models" / "thr.json"
 
 # Remote fallback (raw GitHub link!)
-# MODEL_URL = "https://raw.githubusercontent.com/TestimOnyOni/histopathology_app/main/models/best_resnet50_balanced.pth"
+MODEL_URL = "https://raw.githubusercontent.com/TestimOnyOni/histopathology_app/main/models/best_resnet50_balanced.pth"
 # https://github.com/TestimOnyOni/histoPathologySystem/blob/main/best_resnet50_balanced.pth
-MODEL_URL = "https://raw.githubusercontent.com/TestimOnyOni/histoPathologySystem/blob/main/best_resnet50_balanced.pth"
+# MODEL_URL = "https://raw.githubusercontent.com/TestimOnyOni/histoPathologySystem/blob/main/best_resnet50_balanced.pth"
